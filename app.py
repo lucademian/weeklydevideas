88a639b231cfb2457fa7381629e41e58cb2ecb8f
lucadem1313 <lucadem1313@gmail.com>
2016-07-18 02:32:32 -0400

    adding mvp

---
+++ app.py
@@ -0,0 +1,242 @@
+from flask import Flask, render_template, redirect, jsonify, request
+from flaskext.mysql import MySQL
+import random
+
+mysql = MySQL()
+app = Flask(__name__)
+
+# Set up MySQL Connection Variables
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'ideas'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+# define connection variables
+db = mysql.connect()
+cursor = db.cursor()
+
+currentWeek = 1
+
+def generateString(length):
+    return ''.join(random.choice('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz') for i in range(length))
+
+def verify_user(userId, passKey):
+    cursor.execute("SELECT * FROM `users` WHERE `longid`=%s AND `passkey`=%s", (userId, passKey))
+    if cursor.rowcount > 0:
+        success = True
+        userNumId = cursor.fetchone()[0]
+    else:
+        success = False
+        userNumId = None
+
+    return {"success":success, "userNumId":userNumId}
+
+
+@app.route("/")
+def vote_page():
+    return render_template("vote.html", week=currentWeek)
+
+
+@app.route("/week/<weekNum>")
+def week_page(weekNum):
+    return render_template("vote.html", week=weekNum)
+
+@app.route("/api/ideas/current/", methods = ["GET"])
+@app.route("/api/ideas/week/<weekNum>/", methods = ["GET"])
+def get_ideas(weekNum=currentWeek):
+
+    items = []
+
+    userId = request.args.get("userId")
+    passKey = request.args.get("passKey")
+
+    cursor.execute("SELECT id, title, text, longid, unix_timestamp(date), week FROM `ideas` WHERE week=%s ORDER BY date DESC", (weekNum,))
+    rawItems = cursor.fetchall()
+
+    verifyuser = verify_user(userId, passKey)
+    userNumId = verifyuser["userNumId"]
+
+    if verifyuser["success"]:
+        for item in rawItems:
+            cursor.execute("SELECT * FROM `upvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId,item[0]))
+
+            vote = "neutral"
+
+            if cursor.rowcount > 0:
+                vote = "upvoted"
+            else:
+                cursor.execute("SELECT * FROM `downvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId,item[0]))
+                if cursor.rowcount > 0:
+                    vote = "downvoted"
+
+            cursor.execute("SELECT itemid FROM `upvotes` WHERE `itemid`=%s", (item[0],))
+            upvotes = cursor.rowcount
+
+            cursor.execute("SELECT itemid FROM `downvotes` WHERE `itemid`=%s", (item[0],))
+            downvotes = cursor.rowcount
+
+            karma = upvotes - downvotes
+
+            items.append({"id":item[3], "title":item[1], "text":item[2], "date":item[4], "vote":vote, "karma":karma})
+
+        return jsonify({"success":True, "error":"", "payload":{"items":items}})
+    else:
+        return jsonify({"success":False, "error":"Unable to verify user", "payload":{}})
+
+
+@app.route("/api/user/", methods = ["POST"])
+def create_user():
+    success = False
+    error = ""
+    payload = {}
+
+    userLongId = generateString(60)
+    cursor.execute("SELECT `longid` FROM `users` WHERE `longid`=%s", (userLongId,))
+    numRows = cursor.rowcount
+
+    while numRows > 0:
+        userLongId = generateString(60)
+        cursor.execute("SELECT `longid` FROM users WHERE `longid`=%s", (userLongId,))
+        numRows = cursor.rowcount
+
+    passKey = generateString(15)
+    cursor.execute("INSERT INTO `users` (`longid`, `passkey`) VALUES (%s, %s)", (userLongId,passKey))
+    db.commit()
+
+    success = True
+    error = ""
+    payload = {"userId":userLongId, "passKey":passKey}
+
+    return jsonify({"success":success, "error":error, "payload":payload})
+
+
+
+@app.route("/api/<itemId>/unvote/", methods = ["POST"])
+def unvote(itemId):
+    userId = request.form["userId"]
+    passKey = request.form["passKey"]
+
+    verifyuser = verify_user(userId, passKey)
+
+    if verifyuser["success"]:
+        success = True
+        error = ""
+        userNumId = verifyuser["userNumId"]
+
+        cursor.execute("SELECT `id`, `longid` FROM `ideas` WHERE `longid`=%s", (itemId,))
+        itemNumId = cursor.fetchone()[0]
+
+        cursor.execute("DELETE FROM `upvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId, itemNumId))
+        cursor.execute("DELETE FROM `downvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId, itemNumId))
+        db.commit()
+    else:
+        success = False
+        error = "Unable to verify user."
+
+    return jsonify({"success":success, "error":error})
+
+
+
+@app.route("/api/<itemId>/upvote/", methods = ["POST"])
+def upvote(itemId):
+    userId = request.form["userId"]
+    passKey = request.form["passKey"]
+
+    verifyuser = verify_user(userId, passKey)
+
+    if verifyuser["success"]:
+        success = True
+        error = ""
+        userNumId = verifyuser["userNumId"]
+
+        cursor.execute("SELECT `id`, `longid` FROM `ideas` WHERE `longid`=%s", (itemId,))
+        itemNumId = cursor.fetchone()[0]
+
+        cursor.execute("DELETE FROM `upvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId, itemNumId))
+        cursor.execute("DELETE FROM `downvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId, itemNumId))
+        cursor.execute("INSERT INTO `upvotes` (`userid`, `itemid`) VALUES (%s, %s)", (userNumId, itemNumId))
+        db.commit()
+    else:
+        success = False
+        error = "Unable to verify user."
+
+    return jsonify({"success":success, "error":error})
+
+
+
+@app.route("/api/<itemId>/downvote/", methods = ["POST"])
+def downvote(itemId):
+    userId = request.form["userId"]
+    passKey = request.form["passKey"]
+
+    verifyuser = verify_user(userId, passKey)
+
+    if verifyuser["success"]:
+        success = True
+        error = ""
+        userNumId = verifyuser["userNumId"]
+
+        cursor.execute("SELECT `id`, `longid` FROM `ideas` WHERE `longid`=%s", (itemId,))
+        itemNumId = cursor.fetchone()[0]
+
+        cursor.execute("DELETE FROM `upvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId, itemNumId))
+        cursor.execute("DELETE FROM `downvotes` WHERE `userid`=%s AND `itemid`=%s", (userNumId, itemNumId))
+        cursor.execute("INSERT INTO `downvotes` (`userid`, `itemid`) VALUES (%s, %s)", (userNumId, itemNumId))
+        db.commit()
+    else:
+        success = False
+        error = "Unable to verify user."
+
+    return jsonify({"success":success, "error":error})
+
+
+
+
+@app.route("/api/idea/", methods = ["POST"])
+def create_idea():
+    success = False
+    error = ""
+    payload = {}
+
+    userId = request.form["userId"]
+    passKey = request.form["passKey"]
+
+    title = request.form["title"]
+    text = request.form["text"]
+
+    longId = generateString(20)
+    cursor.execute("SELECT `longid` FROM `ideas` WHERE `longid`=%s", (longId,))
+    numRows = cursor.rowcount
+
+    while numRows > 0:
+        longId = generateString(20)
+        cursor.execute("SELECT `longid` FROM ideas WHERE `longid`=%s", (longId,))
+        numRows = cursor.rowcount
+
+
+    verifyuser = verify_user(userId, passKey)
+
+    if verifyuser["success"]:
+        success = True
+        error = ""
+        userNumId = verifyuser["userNumId"]
+        if len(title) > 0 and len(text) > 0:
+            cursor.execute("INSERT INTO `ideas` (`title`, `text`, `longid`, `week`) VALUES (%s, %s, %s, %s)", (title, text, longId, currentWeek))
+            insertedId = cursor.lastrowid
+            cursor.execute("INSERT INTO `upvotes` (`userid`, `itemid`) VALUES (%s, %s)", (userNumId, insertedId))
+            db.commit()
+        else:
+            success = False
+            error = "Invalid Submission"
+    else:
+        error = "Unable to verify user"
+
+
+    return jsonify({"success":success, "error":error, "payload":payload})
+
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/scripts/vote.js
@@ -0,0 +1,241 @@
+var userId = localStorage.getItem("userId");
+var passKey = localStorage.getItem("passKey");
+
+var items = [];
+
+if(userId == null || passKey == null)
+{
+  $.ajax({
+    type: "POST",
+    url: "/api/user/",
+    data: {},
+    success: function(response){
+      if(response.success)
+      {
+        userId = response.payload.userId;
+        passKey = response.payload.passKey;
+        localStorage.setItem("userId", userId);
+        localStorage.setItem("passKey", passKey);
+      }
+      else {
+        $("body").html("Unable to connect to server. Reloading...");
+        setTimeout(function(){
+          window.location.reload();
+        },2000);
+      }
+    }
+  });
+}
+
+function listItems(items){
+  $("#list ul").empty();
+  $.each(items, function(key, value){
+    $("#list ul").append('<li class="vote-item '+value.vote+'" data-id="'+value.id+'">\
+      <div class="vote-title">'+value.title+'</div>\
+      <div class="vote-text">'+value.text+'</div>\
+      <div class="vote-actions">\
+        <div class="vote-action upvote"></div>\
+        <div class="vote-count">'+value.karma+'</div>\
+        <div class="vote-action downvote"></div>\
+      </div>\
+    </li>');
+  });
+}
+
+$.ajax({
+  type: "GET",
+  url: "/api/ideas/week/"+weekNum,
+  data: {
+    userId:userId,
+    passKey:passKey
+  },
+  success: function(response){
+    console.log(response);
+    if(response.success)
+    {
+      items = response.payload.items;
+
+      listItems(items);
+    }
+  }
+});
+
+
+
+
+$("body").on("click", ".upvote", function(){
+  element = $(this).parents(".vote-item");
+  voteCount = parseInt(element.find(".vote-count").text());
+
+  if(element.hasClass("upvoted"))
+  {
+    element.find(".vote-count").text(voteCount - 1);
+    element.addClass("neutral").removeClass("upvoted").removeClass("downvoted");
+
+    $.ajax({
+      type: "POST",
+      url: "/api/"+element.data("id")+"/unvote/",
+      data: {
+        userId:userId,
+        passKey:passKey
+      },
+      success: function(response){
+        console.log(response);
+      }
+    });
+  }
+  else if(element.hasClass("downvoted"))
+  {
+    $.ajax({
+      type: "POST",
+      url: "/api/"+element.data("id")+"/upvote/",
+      data: {
+        userId:userId,
+        passKey:passKey
+      },
+      success: function(response){
+        console.log(response);
+      }
+    });
+    element.find(".vote-count").text(voteCount + 2);
+    element.addClass("upvoted").removeClass("downvoted").removeClass("neutral");
+  }
+  else
+  {
+    $.ajax({
+      type: "POST",
+      url: "/api/"+element.data("id")+"/upvote/",
+      data: {
+        userId:userId,
+        passKey:passKey
+      },
+      success: function(response){
+        console.log(response);
+      }
+    });
+    element.find(".vote-count").text(voteCount + 1);
+    element.addClass("upvoted").removeClass("downvoted").removeClass("neutral");
+  }
+
+});
+
+$("body").on("click", ".downvote", function(){
+  element = $(this).parents(".vote-item");
+  voteCount = parseInt(element.find(".vote-count").text());
+
+  if(element.hasClass("downvoted"))
+  {
+    element.find(".vote-count").text(voteCount + 1);
+    element.addClass("neutral").removeClass("upvoted").removeClass("downvoted");
+
+    $.ajax({
+      type: "POST",
+      url: "/api/"+element.data("id")+"/unvote/",
+      data: {
+        userId:userId,
+        passKey:passKey
+      },
+      success: function(response){
+        console.log(response);
+      }
+    });
+  }
+  else if(element.hasClass("upvoted"))
+  {
+    $.ajax({
+      type: "POST",
+      url: "/api/"+element.data("id")+"/downvote/",
+      data: {
+        userId:userId,
+        passKey:passKey
+      },
+      success: function(response){
+        console.log(response);
+      }
+    });
+    element.find(".vote-count").text(voteCount - 2);
+    element.addClass("downvoted").removeClass("upvoted").removeClass("neutral");
+  }
+  else
+  {
+    $.ajax({
+      type: "POST",
+      url: "/api/"+element.data("id")+"/downvote/",
+      data: {
+        userId:userId,
+        passKey:passKey
+      },
+      success: function(response){
+        console.log(response);
+      }
+    });
+    element.find(".vote-count").text(voteCount - 1);
+    element.addClass("downvoted").removeClass("upvoted").removeClass("neutral");
+  }
+
+	$(this).toggleClass("active");
+	$(".upvote").removeClass("active");
+});
+
+
+
+$("#sortby").change(function(){
+  if($(this).val() == "newest")
+  {
+    items.sort(function(a, b){
+      return b.date - a.date;
+    });
+    listItems(items);
+  }
+  else if($(this).val() == "oldest")
+  {
+    items.sort(function(a, b){
+      return a.date - b.date;
+    });
+    listItems(items);
+  }
+  else if($(this).val() == "top")
+  {
+    items.sort(function(a, b){
+      return b.karma - a.karma;
+    });
+    listItems(items);
+  }
+});
+
+
+$("#newidea").submit(function(e){
+  e.preventDefault();
+
+  element = $(this);
+  $.ajax({
+    type: "POST",
+    url: "/api/idea/",
+    data: {
+      userId:userId,
+      passKey:passKey,
+      title:element.find("input").val(),
+      text:element.find("textarea").val()
+    },
+    success: function(response){
+      if(response.success)
+      {
+        $("#newidea button").after("<div class='error'>Saved Idea</div>");
+        $("#newidea .error").delay(1500).fadeOut(500, function(){
+          $(this).remove();
+        });
+
+        element.find("input").val("");
+        element.find("textarea").val("");
+      }
+      else
+      {
+        $("#newidea .error").remove();
+        $("#newidea button").after("<div class='error'>Error Saving</div>");
+        $("#newidea .error").delay(1500).fadeOut(500, function(){
+          $(this).remove();
+        });
+      }
+    }
+  });
+});
